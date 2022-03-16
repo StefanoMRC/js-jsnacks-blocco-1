24cddd7bcdc8c4645717b535e1b6a60629df6fd2
StefanoMRC <stefano.mrc92@gmail.com>
2022-03-16 12:17:40 +0100

    fine snack 2

---
+++ JSnack2/main.js
@@ -0,0 +1,10 @@
+let testo1 = prompt("inserisci testo1")
+let testo2 = prompt("inserisci testo 2")
+lunghezza1 = testo1.length
+lunghezza2 = testo2.length
+
+if(lunghezza1 > lunghezza2){
+    document.getElementById("cont").innerHTML=`<h1>${testo1} ${testo2}</h1>`
+} else{
+    document.getElementById("cont").innerHTML=`<h1>${testo2} ${testo1} </h1>`
+}
