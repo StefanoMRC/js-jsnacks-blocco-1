9aa92c98f997bb002694303524ea7aa610511c07
StefanoMRC <stefano.mrc92@gmail.com>
2022-03-16 12:20:23 +0100

    invertiti testi

--- JSnack2/main.js
+++ JSnack2/main.js
@@ -4,7 +4,7 @@ lunghezza1 = testo1.length
 lunghezza2 = testo2.length
 
 if(lunghezza1 > lunghezza2){
-    document.getElementById("cont").innerHTML=`<h1>${testo1} ${testo2}</h1>`
+    document.getElementById("cont").innerHTML=`<h1>${testo2} ${testo1}</h1>`
 } else{
-    document.getElementById("cont").innerHTML=`<h1>${testo2} ${testo1} </h1>`
+    document.getElementById("cont").innerHTML=`<h1>${testo1} ${testo2}</h1>`
 }
